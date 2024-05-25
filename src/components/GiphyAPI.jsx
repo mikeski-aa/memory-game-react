119396fb5f80c8b49b0d122cb296ab9315bdb8e7
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-05-25 15:49:33 +0200

    Working on fetch from Giphy API

---
+++ src/components/GiphyAPI.jsx
@@ -0,0 +1,28 @@
+// this function is to fetch data from Giphy's API
+
+async function getPicture(searchItem) {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=Zf0QL7k2j2O0uwHY5cpCXjxDeUPRcTjs&q=${searchItem}&limit=1&offset=0&rating=pg-13&lang=en&bundle=messaging_non_clips`,
+      { mode: "cors" }
+    );
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    } else {
+      const json = await response.json();
+
+      return json;
+    }
+  } catch (error) {
+    alert("Ooops something went wrong with Giphy's API " + error);
+  }
+}
+
+function displayResult(searchItem) {
+  getPicture(searchItem).then((json) => {
+    console.log(json);
+  });
+}
+
+export { displayResult };
